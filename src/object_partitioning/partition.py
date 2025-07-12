ef57f6a421dfaf08a744c9a40b755b5a50777695
Gordon Watts <gwatts@uw.edu>
2025-07-12 16:01:45 -0400

    Add output file option

--- src/object_partitioning/partition.py
+++ src/object_partitioning/partition.py
@@ -7,12 +7,18 @@ app = typer.Typer()
 
 
 @app.command()
-def main(ds_name: str = typer.Argument(..., help="Name of the dataset")):
+def main(
+    ds_name: str = typer.Argument(..., help="Name of the dataset"),
+    output_file: str = typer.Option(
+        "object_counts.parquet",
+        "--output",
+        "-o",
+        help="Output file name for the object counts parquet file.",
+    ),
+):
     """object-partitioning CLI is working!"""
-
     counts = collect_object_counts(ds_name)
-
-    ak.to_parquet(counts, "object_counts.parquet")
+    ak.to_parquet(counts, output_file)
 
 
 if __name__ == "__main__":
